b87744d3638d05945a8eb8114ada0ea9a1633c16
BogdanBarylo <barylob@gmail.com>
2024-11-20 13:12:53 +0200

    fix

--- chat/conf.py
+++ chat/conf.py
@@ -9,7 +9,7 @@ load_dotenv()
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="APP_")
     redis_url: str = Field(env="REDIS_URL")
-    redis_test_url: str = Field(env="REDIS_TEST_URL")
+
 
 settings = Settings()
 redis = aioredis.from_url(settings.redis_url, decode_responses=True)
